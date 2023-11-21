1b5bff5ec99e8ffcd9541344edade9b89c326a08
yellpall <ohj3423@naver.com>
2023-11-22 07:59:25 +0900

    세팅 파일 업로드

---
+++ src/main/java/com/third/gachigong/GachigongApplication.java
@@ -0,0 +1,13 @@
+package com.third.gachigong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachigongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachigongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/third/gachigong/GachigongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.third.gachigong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachigongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
