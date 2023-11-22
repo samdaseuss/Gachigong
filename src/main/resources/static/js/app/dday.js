df5c365d278fbb737b529c4873d15f7363b61316
yellpall <ohj3423@naver.com>
2023-11-22 18:32:05 +0900

    README.md

--- src/main/resources/static/js/app/dday.js
+++
@@ -1,5 +0,0 @@
-var date = document.getElementById("originalDate").innerText;
-var currentDate = new Date();
-var originalDate = new Date(date);
-var ddayValue = Math.floor((currentDate - originalDate) / (1000 * 60 * 60 * 24));
-document.getElementById('origcinalDate').innerText = 'D-Day: ' + ddayValue + ' days';
