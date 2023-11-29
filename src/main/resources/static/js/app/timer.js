e1dad33cc58e9c63fdc81c4b632fcec1a9764e20
yellpall <ohj3423@naver.com>
2023-11-29 22:28:34 +0900

    UI 수정

--- src/main/resources/static/js/app/timer.js
+++ src/main/resources/static/js/app/timer.js
@@ -1,20 +1,55 @@
-//var timerElement = document.getElementById('timer');
-//var isRunning = false;
-//var startTime;
-//var elapsedTime = 0;
+// totalTime 값을 가져오기
+var total = document.getElementById('totalTime');
+var totalTime = total.textContent || total.innerText;
+
+// totalTime을 시간으로 변환하여 총 시간을 계산
+var totalHours = parseInt(totalTime.split(':')[0]);
+// 페이지 로딩 시 이미지 변경 함수 호출
+window.onload = changeImage;
+
+// 이미지 변경을 위한 함수
+function changeImage() {
+    var imgElement = document.getElementById('timeImage');
+
+
+    if (totalHours >= 1) {
+        imgElement.src = "/img/img_1.png";
+    }
+    else if (totalHours >= 2) {
+        imgElement.src = "/img/img_2.png";
+    }
+    else if (totalHours >= 3) {
+        imgElement.src = "/img/img_3.png";
+    }
+}
+
+
+
 var timers = {};
 timers.isRunning = false;
 
 function startStop(id) {
+    var button = document.querySelector(`#controls button[data-id="${id}"]`);
     if (!timers[id] || !timers[id].isRunning) {
         start(id);
+        button.textContent = "︎︎◼︎";
+        button.style.backgroundColor = "#FF6757";
     } else {
         stop(id);
+        button.textContent = "▶︎";
+        button.style.backgroundColor = "#5A5A5A";
+        sendData(id);
     }
 }
 
 function start(id) {
+    //id값이 timer_id에 있는 String 값을 가져와서 밀리초 단위로 변환해서 DB에 있는 시간 정보 이어서 감
+    var timerElement = document.getElementById('timer_' + id);
+    var saveTime = timerElement.textContent || timerElement.innerText;
+    var milliseconds = convertToMilliseconds(saveTime);
     timers[id] = timers[id] || {};
+    timers[id].elapsedTime = milliseconds;
+
     timers[id].isRunning = true;
     timers[id].startTime = Date.now() - (timers[id].elapsedTime || 0);
     // if (!timers[id].elapsedTime) {
@@ -53,6 +88,18 @@ function formatTime(milliseconds) {
     return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
 }
 
+
+function convertToMilliseconds(formattedTime) {
+    var timeArray = formattedTime.split(':');
+    var hours = parseInt(timeArray[0], 10);
+    var minutes = parseInt(timeArray[1], 10);
+    var seconds = parseInt(timeArray[2], 10);
+
+    var totalMilliseconds = (hours * 3600 + minutes * 60 + seconds) * 1000;
+
+    return totalMilliseconds;
+}
+
 function pad(number) {
     return (number < 10 ? '0' : '') + number;
 }
@@ -76,4 +123,15 @@ function sendData(id) {
             }
         }
     });
+}
+
+function toggleStartStop(id) {
+    var button = document.querySelector(`#controls button[data-id="${id}"]`);
+
+    if (button.textContent === "▶︎") {
+        button.textContent = "︎︎◼︎";
+        startStop(id);
+    } else {
+        button.textContent = "▶︎";
+    }
 }
--- src/main/resources/static/js/app/todo.js
+++ src/main/resources/static/js/app/todo.js
@@ -2,7 +2,9 @@ const inputField = document.querySelector(".input-field input");
 const todoLists = document.querySelector(".todoLists");
 const clearButton = document.querySelector(".clear");
 
-function allTasks() { let tasks = document.querySelectorAll(".pending"); }
+function allTasks() {
+    let tasks = document.querySelectorAll(".pending");
+}
 
 inputField.addEventListener("keyup", (e) => {
     let inputVal = inputField.value.trim();
@@ -10,7 +12,7 @@ inputField.addEventListener("keyup", (e) => {
         let list = `<div class="input-group mb-3">
                             <span class="input-group-text"><input class="form-check-input mt-0" type="checkbox" value="" aria-label=""></span>
                             <span class="form-control">${inputVal}</span>
-                            <span class="input-group-text btn-danger" onclick="deleteTask(e)">X</span>
+                            <span class="input-group-text btn-danger" onclick="deleteTask(this)">X</span>
                     </div>`
         todoLists.insertAdjacentHTML("beforeend", list);
         inputField.value = "";
@@ -24,4 +26,6 @@ function handleStatus(e) {
     e.classList.toggle("pending");
 }
 
-deleteTask(e) => e.parentElement.remove;
+function deleteTask(e) {
+    e.parentElement.remove();
+}
